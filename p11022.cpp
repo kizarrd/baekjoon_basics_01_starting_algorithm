6240d781ef2696053c21104368eb67558f817d0f
kizarrd <51534324+kizarrd@users.noreply.github.com>
2020-06-18 23:43:47 +0900

    input output practice

---
+++ p11022.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int t;
+    int a, b;
+    int i = 1;
+
+    scanf("%d", &t);
+    while (t--)
+    {
+        scanf("%d %d", &a, &b);
+        printf("Case #%d: %d + %d = %d\n", i, a, b, a + b);
+        i++;
+    }
+
+    return 0;
+}
